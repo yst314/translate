dbf619423774460552db017ddfcbe7e860ba64d7
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2023-12-05 16:35:10 +0900

    Add translation with gpt

---
+++ test_llm.py
@@ -0,0 +1,27 @@
+import ocr
+import time
+
+from PIL import Image
+
+from translate_llm import GptTransaltor
+from capture import capture
+from region import select_region
+
+gpt = GptTransaltor()
+while True:
+    region = select_region()
+    im = capture(region)
+
+    start = time.time()
+    txt = ocr.ocr(im)
+    time_ocr = time.time() - start
+
+    # txt = txt.replace("\n", " ")
+
+    print(txt)
+    print("-----------------------------------")
+    start = time.time()
+    ja = gpt.translate(txt)
+    time_translate = time.time() - start
+
+    print(f"ocr: {time_ocr}, translate: {time_translate}")
---
+++ translate_llm.py
@@ -0,0 +1,36 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import HumanMessage
+from dotenv import load_dotenv
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+class GptTransaltor:
+    def __init__(self):
+        self.llm = ChatOpenAI(
+            temperature=0, 
+            model_name="gpt-3.5-turbo", 
+            streaming=True,
+            api_key=OPENAI_API_KEY,
+            callbacks=[StreamingStdOutCallbackHandler()]
+        )
+        template = """日本語に翻訳してください.
+        変換前:{input}
+        変換後:"""
+        self.prompt = PromptTemplate(template=template, input_variables=["input"])
+
+    def translate(self, text):
+        result = self.llm.invoke(
+            [
+                HumanMessage(content=self.prompt.format(input=text))
+            ]
+        )
+        return result
+
+if __name__ == "__main__":
+    gpt = GptTransaltor()
+    result = gpt.translate("Undistorted images from D2Net are not available anymore.For a temporal alternative, please use the undistorted images provided by the MegaDepth_v1 (should be downloaded along with the required depth files). ")
+    print(result)
