ceefab6111e1cc82f79e67128135c575d2515d7b
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2023-12-06 17:06:44 +0900

    Update

--- ui.py
+++ ui.py
@@ -1,22 +1,21 @@
 import time
-
+import os
 import streamlit as st
 
+st.write("Hello, World")
+st.markdown("# Translator")
 
-def main():
-    status_text = st.empty()
-    # プログレスバー
-    progress_bar = st.progress(0)
+def get_last_modified_time(folder_path):
+    return max(os.path.getmtime(os.path.join(folder_path, f)) for f in os.listdir(folder_path))
 
-    for i in range(100):
-        status_text.text(f'Progress: {i}%')
-        # for ループ内でプログレスバーの状態を更新する
-        progress_bar.progress(i + 1)
-        time.sleep(0.1)
+folder_path = 'results'
+last_modified_time = get_last_modified_time(folder_path)
 
-    status_text.text('Done!')
-    st.balloons()
-
-
-if __name__ == '__main__':
-    main()
+# Streamlitアプリのメインループ
+while True:
+    time.sleep(1)  # 1秒ごとにチェック
+    new_modified_time = get_last_modified_time(folder_path)
+    if new_modified_time > last_modified_time:
+        last_modified_time = new_modified_time
+        # ここでStreamlitアプリの表示を更新
+        st.experimental_rerun()
