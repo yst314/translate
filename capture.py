d89c2be5d9bcc77a2b2724b6d9e7826410af9ee2
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2023-12-06 16:28:17 +0900

    Update

--- capture.py
+++ capture.py
@@ -3,7 +3,10 @@ from region import Region
 
 def capture(region:Region):
     x1, y1, x2, y2 = region.get_points()
-    im = pyautogui.screenshot("screenshot.jpg", region=(x1,y1, x2-x1,y2-y1))
+    try:
+        im = pyautogui.screenshot("screenshot.jpg", region=(x1,y1, x2-x1,y2-y1))
+    except ValueError as e:
+        raise e
     return im 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -4,6 +4,11 @@ def load_config():
     with open('config.yml', 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+def save_yaml(data, path):
+    with open(path, "w", encoding="UTF-8") as f:
+        yaml.dump(data, f, allow_unicode=True)
+
 if __name__ == '__main__':
     config = load_config()
     print(config)
